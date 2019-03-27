f60e00bfb2a49ee39bb5f5084fa7dfa6ce5041d6
DmitriTarasov <dmta9796@colorado.edu>
2019-03-27 12:41:38 -0600

    boilerplate code

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>Hi from react app</div>
+        )
+    }
+}
+export default App;
