215f14770ec6e14d7247a8259f49ef97d7a03f85
DmitriTarasov <dmta9796@colorado.edu>
2019-03-28 12:13:55 -0600

    eslint

--- database/schema.js
+++ database/schema.js
@@ -1,46 +1,45 @@
 var mongoose = require('mongoose');
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 mongoose.Promise = require('bluebird');
 var Schema = mongoose.Schema;
 
 var artistSchema = new Schema({
   name: String,
-  bio:  String,
+  bio: String,
   image: String,
   relatedartists:[]
 });
 
 var Artist = mongoose.model('Artist', artistSchema);
 
-var fetchImage=function(){
-  const imagewidth=400;
-  const imageheight=480;
-  const collectionid=1163637;
-  return fetch(`http://source.unsplash.com/collection/${collectionid}/${imagewidth}x${imageheight}`)
-  .then((response)=>{
+var fetchImage = function() {
+  const imagewidth = 400;
+  const imageheight = 480;
+  const collectionid = 1163637;
+  return fetch(`http://source.unsplash.com/collection/${collectionid}/${imagewidth}x${imageheight}`).then((response)=>{
     return response.url;
-  })
-}
+  });
+};
 
-var seeddata=()=>{
+var seeddata = ()=>{
   mongoose.connect('mongodb://localhost/artists');
   Artist.collection.drop();
-  var imageurls=[];
-  var count=0;
-  var entries=100;
-  while( count<entries){
+  var imageurls = [];
+  var count = 0;
+  var entries = 100;
+  while (count < entries) {
     imageurls.push(fetchImage());
-    count=count+1;
+    count = count + 1;
   }
   Promise.all(imageurls).then((imageurls)=>{
-    for(var index in imageurls){
-      index=parseInt(index);
-      var image=imageurls[index]
+    for (var index in imageurls) {
+      index = parseInt(index);
+      var image = imageurls[index];
       console.log(image);
-      Artist.create({name:`${index}`,bio:`bio of ${index}`,image:image,relatedartists:[(index+1%100),(index+20)%100]})
+      Artist.create({name: `${index}`, bio: `bio of ${index}`, image: image, relatedartists: [(index + 1) % 100, (index + 20) % 100]});
     }
-  })
-}
+  });
+};
 
 //seeddata();
-module.exports={seeddata,fetchImage};
+module.exports = {seeddata,fetchImage};
