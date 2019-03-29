f84d28ba35b6b0b3503fe76ec964aaf1c78c7eda
DmitriTarasov <dmta9796@colorado.edu>
2019-03-29 11:16:08 -0600

    es lint configs set for jest & react (from the look of it, it seems like the linter is setup properly)

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-class App extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return (
-            <div><div>Hi from react app</div></div>
-        )
-    }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Hi from react app</div>
+    );
+  }
 }
-module.exports={App:App};
+export default App;
--- src/database/schema.js
+++ src/database/schema.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const fetch = require('node-fetch');
-mongoose.Promise = require('bluebird');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const artistSchema = new Schema({
   name: String,
@@ -17,7 +16,7 @@ const fetchImage = () => {
   const imagewidth = 400;
   const imageheight = 480;
   const collectionid = 1163637;
-  return fetch(`http://source.unsplash.com/collection/${collectionid}/${imagewidth}x${imageheight}`).then((response) => { return response.url; });
+  return fetch(`http://source.unsplash.com/collection/${collectionid}/${imagewidth}x${imageheight}`).then((response) => response.url);
 };
 
 const seeddata = () => {
@@ -30,12 +29,13 @@ const seeddata = () => {
     imageurls.push(fetchImage());
     count += 1;
   }
-  Promise.all(imageurls).then((imageurls)=>{
-    for (var index in imageurls) {
-      index = parseInt(index);
-      var image = imageurls[index];
-      Artist.create({name: `${index}`, bio: `bio of ${index}`, image: image, relatedartists: [(index + 1) % 100, (index + 20) % 100]});
-    }
+  Promise.all(imageurls).then((images) => {
+    const imagesarray = Object.values(images);
+    imagesarray.forEach((image, index) => {
+      Artist.create({
+        name: `${index}`, bio: `bio of ${index}`, image, relatedartists: [(index + 1) % 100, (index + 20) % 100],
+      });
+    });
   });
 };
 
