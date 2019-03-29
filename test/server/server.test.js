2a39e3804af9c9c828b4080202d22353a62c5ce9
DmitriTarasov <dmta9796@colorado.edu>
2019-03-29 12:25:41 -0600

    throw away test for server

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -1,7 +1,3 @@
 const fetch = require('node-fetch');
 
-test('test to see if server works', () => {
-  return fetch('http://localhost:3000').then((data) => {
-    expect(!!data).toBe(true);
-  });
-});
+test('test to see if server works', () => fetch('http://localhost:3000').then((data) => { expect(!!data).toBe(true); }));
