77fe8bf66da75d07921ec3c29eaa15df60bf96eb
DmitriTarasov <dmta9796@colorado.edu>
2019-04-08 11:59:54 -0600

    context menu position change

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -59,7 +59,7 @@ class App extends React.Component {
     const classtype = event.target.className.split('__')[0];
     if (classtype === 'Artist') {
       this.setState({ showmenu: true });
-      const newpos = { left: event.clientX, top: event.clientY };
+      const newpos = { left: event.clientX + 10, top: event.clientY + 10 };
       this.setState({ menuposition: newpos });
     } else {
       this.setState({ showmenu: false });
