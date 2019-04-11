5ab64ff779fa02538c6c988fa58b08875acb7d0b
DmitriTarasov <dmta9796@colorado.edu>
2019-04-11 12:51:48 -0600

    cors middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const db = require('../database/schema');
@@ -9,6 +10,7 @@ const port = 3100;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.use(express.static(path.join(__dirname, '/../../public')));
 app.use(express.json());
+app.use(cors());
 
 
 app.get('/artists', (req, res) => {
