f25b84c1fabfe28284d8b8c9436347e84e124315
DmitriTarasov <dmta9796@colorado.edu>
2019-04-16 16:41:06 -0600

    forgotten stuff

--- src/server/index.js
+++ src/server/index.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3100;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/../../public')));
 app.use(express.json());
-app.use(cors());
 
 
 app.get('/artists', (req, res) => {
